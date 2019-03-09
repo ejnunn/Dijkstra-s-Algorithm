879ee841d0d02509319b3ce544b1ae150eb87d63
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2019-03-08 19:26:55 -0800

    Outlined Graph class, and started implementing functionality.

--- src/main/Graph.java
+++ src/main/Graph.java
@@ -1,5 +1,59 @@
 package main;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
+
+/**
+ * 
+ * @author ericnunn
+ *
+ */
 public class Graph {
-
-}
+	private Map<String, Map<String, Integer>> adjList;
+	
+	/**
+	 * 
+	 */
+	public Graph() {
+		this.adjList = new HashMap<>();
+	} // end constructor
+	
+	/**
+	 * 
+	 * @param vertex_from
+	 * @param vertex_to
+	 * @param weight
+	 */
+	public void addEdge(String vertex_from, String vertex_to, Integer weight) {
+		this.adjList.get(vertex_from).put(vertex_to, weight);
+	} // end addEdge
+	
+	/**
+	 * 
+	 * @param vertex_from
+	 * @param vertex_to
+	 */
+	public void addEdge(String vertex_from, String vertex_to) {
+		Integer weight = 1;
+		this.adjList.get(vertex_from).put(vertex_to, weight);
+	} // end addEdge
+	
+	/**
+	 * 
+	 * @param vertex
+	 * @return
+	 */
+	public ArrayList<String> outgoing(String vertex) {
+		return new ArrayList<>(this.adjList.get(vertex).keySet());
+	} // end outgoing
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<String> vertices() {
+		return new ArrayList<>(this.adjList.keySet());
+	} // end vertices
+} // end Graph
