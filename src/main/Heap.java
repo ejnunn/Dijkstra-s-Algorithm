3a4fce26893b93a5b66e1814f6530d064f5e486c
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2019-03-08 18:31:23 -0800

    Finished adding functionality to Heap class.

--- src/main/Heap.java
+++ src/main/Heap.java
@@ -22,7 +22,7 @@ public class Heap {
 	
 	/**
 	 * Builds a min heap from an initial HashMap, and keeps track
-	 * of the location of each item in the heap so that its wieght
+	 * of the location of each item in the heap so that its weight
 	 * can be changed (and the heap repaired in 0(log n) time.
 	 * @param initial HashMap to heapify
 	 */
@@ -116,6 +116,11 @@ public class Heap {
 		}
 	} // end heapConstruct
 	
+	/**
+	 * Returns the weight of the given integer. Null is returned if i is not in the heap.
+	 * @param i	index of element
+	 * @return the weight of the ith element, if it exists
+	 */
 	private Integer weight(Integer i) {
 		if (i > this.last()) {
 			return null;
@@ -123,25 +128,79 @@ public class Heap {
 		return this.weights.get(this.heap.get(i));
 	}
 	
+	/**
+	 * Returns the last index of the heap
+	 * @return last index of the heap
+	 */
 	private Integer last() {
 		return this.heap.size() - 1;
 	} // end last
 	
+	/**
+	 * Returns the index of the parent node in the heap
+	 * @param i	index of element
+	 * @return index of the ith element's parent
+	 */
 	private Integer parent(Integer i) {
 		return (i-1) >> 1;
 	}
 	
+	/**
+	 * Returns a Pair object of both children of the given element in the heap.
+	 * @param p
+	 * @return children elements pair
+	 */
 	private Pair<Integer, Integer> children(Integer p) {
 		Integer left = (p<<1) + 1;
 		Integer right = left + 1;
 		return new Pair<Integer, Integer>(left, right);
 	}
 	
+	/**
+	 * Recursively swap elements up the heap to restore min-heap properties.
+	 * @param i	element to swap up the heap
+	 */
 	private void swapUp(Integer i) {
-		
-	}
+		if (i > 0) {
+			Integer p = this.parent(i);
+			if (this.weight(i) < this.weight(p)) {
+				String temp = this.heap.get(p);
+				this.heap.add(p, this.heap.get(i));
+				this.heap.add(i, temp);
+				this.place.put(this.heap.get(i), i);
+				this.place.put(this.heap.get(p), p);
+				this.swapUp(p);
+			}
+		}
+	} // end swapUp
 	
-	private void swapDown(Integer i) {
+	/**
+	 * Recursively swap elements down the heap to restore min-heap properties.
+	 * @param p	element to swap down the heap
+	 */
+	private void swapDown(Integer p) {
+		Integer child = this.children(p).getKey();
+		Integer right = this.children(p).getValue();
 		
-	}
+		Integer child_w = this.weight(child);
+		if (child_w == null) {
+			return;
+		}
+		
+		Integer right_w = this.weight(right);
+		if (right_w != null && right_w < child_w) {
+			child = right;
+			child_w = right_w;
+		}
+		
+		if (this.weight(p) > child_w) {
+			String temp = this.heap.get(child);
+			this.heap.set(child, this.heap.get(p));
+			this.heap.set(p, temp);
+			this.place.put(this.heap.get(child), child);
+			this.place.put(this.heap.get(p), p);
+			this.swapDown(child);
+		}
+		
+	} // end swapDown
 } // end Heap
